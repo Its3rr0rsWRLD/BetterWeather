ba29be483f78fa4eaf0133e1f3713ade3a9f202b
Error 404 <school-exploits@protonmail.com>
2023-07-23 00:00:40 -0500

    Just adding stuff :)

--- src/main/java/org/thaterror404/betterweather/BetterWeather.java
+++ src/main/java/org/thaterror404/betterweather/BetterWeather.java
@@ -3,7 +3,5 @@ package org.thaterror404.betterweather;
 public class BetterWeather {
     public static void main(String[] args) {
         System.out.println("BetterWeather has been loaded!");
-
-
     }
 }
